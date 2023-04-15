58f98a51565b83f5460d2fcc3b4111aac4169503
Jenish <j.ghimire555@gmail.com>
2023-04-15 15:52:38 -0500

    Moved model controller

--- src/main/java/com/miu/onlinemarketplace/OnlinemarketplaceApplication.java
+++ src/main/java/com/miu/onlinemarketplace/OnlinemarketplaceApplication.java
@@ -1,9 +1,7 @@
 package com.miu.onlinemarketplace;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlinemarketplaceApplication{
@@ -11,9 +9,6 @@ public class OnlinemarketplaceApplication{
     public static void main(String[] args) {
         SpringApplication.run(OnlinemarketplaceApplication.class, args);
     }
-    @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
+
 
 }
--- src/main/java/com/miu/onlinemarketplace/config/AppConfig.java
+++ src/main/java/com/miu/onlinemarketplace/config/AppConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,6 +15,11 @@ import java.util.Arrays;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 
     @Bean
     public ObjectMapper objectMapper() {
