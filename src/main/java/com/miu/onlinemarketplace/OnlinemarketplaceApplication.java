d63b59546c6c205945b1b1bcd6459e885357e2a1
prabeensoti <prabeen.soti@hotmail.com>
2023-04-16 13:47:04 -0500

    Fixed

--- src/main/java/com/miu/onlinemarketplace/OnlinemarketplaceApplication.java
+++ src/main/java/com/miu/onlinemarketplace/OnlinemarketplaceApplication.java
@@ -1,17 +1,22 @@
 package com.miu.onlinemarketplace;
 
 import com.miu.onlinemarketplace.config.AppProperties;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties({AppProperties.class})
-public class OnlinemarketplaceApplication{
+public class OnlinemarketplaceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlinemarketplaceApplication.class, args);
     }
 
-
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
