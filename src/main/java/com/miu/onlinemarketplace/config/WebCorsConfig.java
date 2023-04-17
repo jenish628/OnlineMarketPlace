14733df4de03b18952cf2641578db4ed4c11ea93
ashrawan <ashrawan70@gmail.com>
2023-04-17 10:49:57 -0500

    fixed cors config

--- src/main/java/com/miu/onlinemarketplace/config/WebCorsConfig.java
+++ src/main/java/com/miu/onlinemarketplace/config/WebCorsConfig.java
@@ -1,5 +1,6 @@
 package com.miu.onlinemarketplace.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,10 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebCorsConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AppProperties appProperties;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(new AppProperties().getCors().getAllowedOrigins());
+        configuration.setAllowedOrigins(appProperties.getCors().getAllowedOrigins());
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
